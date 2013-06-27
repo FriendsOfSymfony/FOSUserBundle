6c481dd3a2152fbf52728ad1d0939d6995dd65b2
Sebastian Krebs <krebs.seb@gmail.com>
2013-06-27 21:59:23 +0200

    Properly initialize "groups"-property

--- Model/User.php
+++ Model/User.php
@@ -130,6 +130,7 @@ abstract class User implements UserInterface, GroupableInterface
         $this->expired = false;
         $this->roles = array();
         $this->credentialsExpired = false;
+        $this->groups = new ArrayCollection();
     }
 
     public function addRole($role)
@@ -545,7 +546,7 @@ abstract class User implements UserInterface, GroupableInterface
      */
     public function getGroups()
     {
-        return $this->groups ?: $this->groups = new ArrayCollection();
+        return $this->groups;
     }
 
     public function getGroupNames()
