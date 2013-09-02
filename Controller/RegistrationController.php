ff412670c58eab5f6ba713e14ea3e89794ea43ae
Jonathan Bensaid <john@bensaidj.com>
2013-09-02 18:29:52 +0000

    Add Route and Action for ResendConfirm

--- Controller/RegistrationController.php
+++ Controller/RegistrationController.php
@@ -16,6 +16,8 @@ use FOS\UserBundle\Event\FormEvent;
 use FOS\UserBundle\Event\GetResponseUserEvent;
 use FOS\UserBundle\Event\FilterUserResponseEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\UserBundle\Event\ResendConfirmEvent;
+use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -80,6 +82,54 @@ class RegistrationController extends Controller
         }
 
         return $this->render('FOSUserBundle:Registration:register.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Resend the confirmation email to the User
+     */
+    public function resendConfirmAction(Request $request)
+    {
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->container->get('fos_user.resend_confirm.form.factory');
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->container->get('fos_user.user_manager');
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->container->get('event_dispatcher');
+
+        $user = $userManager->createUser();
+
+        $form = $formFactory->createForm();
+        $form->setData($user);
+
+        if ('POST' === $request->getMethod()) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $email = $user->getEmail();
+                $user = $userManager->findUserByEmail($email);
+
+                if (null === $user) {
+                    throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
+                }
+
+                $event = new GetResponseUserEvent($user, $request);
+                $dispatcher->dispatch(FOSUserEvents::RESEND_CONFIRM, $event);
+
+                $userManager->updateUser($user);
+
+                if (null === $response = $event->getResponse()) {
+                    $url = $this->container->get('router')->generate('fos_user_registration_confirmed');
+                    $response = new RedirectResponse($url);
+                }
+
+                return $response;
+            }
+
+        }
+
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:resend_confirm.html.'.$this->getEngine(), array(
             'form' => $form->createView(),
         ));
     }
