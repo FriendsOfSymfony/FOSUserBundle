17b68cf7eb9dd4fa24e81d74f2a0a42b41fdc0f6
xavierfaucon <xavier.faucon@yoochart.com>
2014-11-30 15:05:05 +0100

    Update EmailUserProvider.php

--- Security/EmailUserProvider.php
+++ Security/EmailUserProvider.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the FOSUserBundle package.
  *
@@ -8,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace FOS\UserBundle\Security;
 
 class EmailUserProvider extends UserProvider
@@ -16,8 +14,8 @@ class EmailUserProvider extends UserProvider
     /**
      * {@inheritDoc}
      */
-    protected function findUser($username)
+    protected function findUser($email)
     {
-        return $this->userManager->findUserByUsernameOrEmail($username);
+        return $this->userManager->findUserByEmail($email);
     }
 }
