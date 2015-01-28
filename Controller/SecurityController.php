320b156da5d561a57aaf44187b2bf68157929ca7
Bocharsky Victor <bocharsky.bw@gmail.com>
2015-01-28 10:14:53 +0200

    Use new security error helper in loginAction
    Use a new helper to drastically reduce the code used in the login form actions, [new for Symfony 2.6](http://symfony.com/blog/new-in-symfony-2-6-security-component-improvements#added-a-security-error-helper)

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -20,34 +20,16 @@ class SecurityController extends Controller
 {
     public function loginAction(Request $request)
     {
-        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
-        $session = $request->getSession();
-
-        // get the error if any (works with forward and redirect -- see below)
-        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContextInterface::AUTHENTICATION_ERROR);
-        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
-            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
-        } else {
-            $error = null;
-        }
-
-        if (!$error instanceof AuthenticationException) {
-            $error = null; // The value does not come from the security component.
-        }
-
-        // last username entered by the user
-        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+        $helper = $this->get('security.authentication_utils');
 
         $csrfToken = $this->has('form.csrf_provider')
             ? $this->get('form.csrf_provider')->generateCsrfToken('authenticate')
             : null;
 
         return $this->renderLogin(array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
-            'csrf_token' => $csrfToken,
+            'last_username' => $helper->getLastUsername(),
+            'error'         => $helper->getLastAuthenticationError(),
+            'csrf_token'    => $csrfToken,
         ));
     }
 
