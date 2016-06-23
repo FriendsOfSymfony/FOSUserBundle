79c225e3a1d986140a4fb57053fd666ab224adfe
Jonny Schmid <hi@jonathanschmid.de>
2016-06-23 18:39:12 +0100

    Amend GroupFormTypeTest to support new RolesFormType

--- Tests/Form/Type/GroupFormTypeTest.php
+++ Tests/Form/Type/GroupFormTypeTest.php
@@ -3,24 +3,49 @@
 namespace FOS\UserBundle\Tests\Form\Type;
 
 use FOS\UserBundle\Form\Type\GroupFormType;
+use FOS\UserBundle\Form\Type\RolesFormType;
 use FOS\UserBundle\Tests\TestGroup;
 use FOS\UserBundle\Util\LegacyFormHelper;
+use FOS\UserBundle\Util\RolesHelper;
+use Symfony\Component\Form\PreloadedExtension;
 
 class GroupFormTypeTest extends TypeTestCase
 {
+    private $rolesHelper;
+
+    protected function setUp()
+    {
+        $this->rolesHelper = new RolesHelper(array());
+
+        parent::setUp();
+    }
+
+    protected function getTypeExtensions()
+    {
+        $extensions = parent::getTypeExtensions();
+
+        $type = new RolesFormType($this->rolesHelper);
+
+        return array_merge($extensions, array(
+            new PreloadedExtension(array($type), array()),
+        ));
+    }
+
     public function testSubmit()
     {
         $group = new TestGroup('foo');
 
         $form = $this->factory->create(LegacyFormHelper::getType('FOS\UserBundle\Form\Type\GroupFormType'), $group);
         $formData = array(
-            'name'      => 'bar',
+            'name' => 'bar',
+            'roles' => array(),
         );
         $form->submit($formData);
 
         $this->assertTrue($form->isSynchronized());
         $this->assertEquals($group, $form->getData());
         $this->assertEquals('bar', $group->getName());
+        $this->assertEquals('roles', array());
     }
 
     protected function getTypes()
