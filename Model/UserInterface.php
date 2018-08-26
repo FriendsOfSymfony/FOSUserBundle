6b8d62b2fb9ca5ae310410d449797e391ec62f94
Julian Finkler <julian@mintware.de>
2018-08-26 09:43:02 +0200

    fos_user.user_checker Service marked as non-public

--- Model/UserInterface.php
+++ Model/UserInterface.php
@@ -15,7 +15,7 @@ use Symfony\Component\Security\Core\User\EquatableInterface;
 use Symfony\Component\Security\Core\User\UserInterface as BaseUserInterface;
 
 /**
- * @internal Only for back compatibility Remove / Merge with UserInterface in Nov 2023 (End of support for security fixes SF 4.4)
+ * @internal Only for back compatibility. Remove / merge when dropping support for Symfony 4
  */
 interface FosUserInterface extends \Serializable
 {
